1b23953809a874b33bd0ff95c223ad437894422b
guimarques7 <gamarques7@gmail.com>
2021-10-20 12:41:23 -0300

    adding BeerList Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { List } from './components/Beer';
 
 function App() {
   return (
     <div className="App">
+      <h1>Cervejas</h1>
+      <List />
     </div>
   );
 }
---
+++ src/components/Beer/List.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function List() {
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/beers')
+      .then((response) => response.json())
+      .then((data) => setBeers(data));
+  }, []);
+
+  return (
+    <ul className="Beer-List">
+      {
+        beers.map((beer) => <li key={beer.id}>{beer.name}</li>)
+      }
+    </ul>
+  );
+}
+
+export default List;
