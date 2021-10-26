16141414162b32bceb38b0742811a584805f64d5
guimarques7 <gamarques7@gmail.com>
2021-10-25 22:13:44 -0300

    updating the list after adding a new beer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import { List, Create } from './components/Beer';
 import { Col, Container, Row } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+  const [refresh, setRefresh] = useState(true);
+
   return (
     <div className="App">
       <Container>
         <Row>
           <Col>
             <h1>Cervejas</h1>
-            <Create />
-            <List />
+            <Create onCreated={() => setRefresh(true)} />
+            <List refresh={refresh} onRefreshed={() => setRefresh(false)} />
           </Col>
         </Row>
       </Container>
